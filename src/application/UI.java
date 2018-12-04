6637fc516a1d192cadca30a8f7d011ea273e2dbf
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2018-12-03 23:57:40 -0200

    Adjustment on the chessboard

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,26 +29,26 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
+		
 		try {
 			String s = sc.nextLine();
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		} 
-		catch (RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
-			} 
+			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
@@ -58,12 +58,11 @@ public class UI {
 		
 		if (piece == null) {
 			System.out.print("-");
-		}
-		else {
+		} else {
 			if (piece.getColor() == Color.WHITE) {
-				System.out.println(ANSI_WHITE + piece + ANSI_RESET);
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
 			} else {
-				System.out.println(ANSI_YELLOW + piece + ANSI_RESET);
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
 			}
 		}
 
